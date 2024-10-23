83d78fa390997ac269839819ffc627aa07503537
servicenowusecase <servicenowusecase@gmail.com>
2024-10-23 16:27:14 +0530

    Update EvenrOdd.js

--- EvenrOdd.js
+++ EvenrOdd.js
@@ -1,7 +1,7 @@
 // 3. Even or Odd Checker
 function isEvenOrOdd(num) {
     if (num % 2 === 0) {
-        return `${num} is Even`;
+        return `${num} is Even number`;
     } else {
         return `${num} is Odd`;
     }
